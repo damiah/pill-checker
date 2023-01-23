3fb0040d49e228740565d6fd8a7c8733ffd8d2cb
damiah <miah.stewart@outlook.com>
2023-01-24 09:01:09 +1300

    commented unneeded line

--- pill-checker/app/api/routes/api.py
+++ pill-checker/app/api/routes/api.py
@@ -11,6 +11,6 @@ router.include_router(pills.router)
 async def root():
     return {"message": "Hello World"}
 
- # at last, the bottom of the file/module
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+#  # at last, the bottom of the file/module
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=8080)
